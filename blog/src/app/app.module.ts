91a224d8fd32d4a7190b0d07cfb8c8529ad91020
Your Name <you@example.com>
2018-10-26 09:13:31 +0200

    Zadania 1-6

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -4,15 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppNavbarComponent } from './components/app-navbar/app-navbar.component';
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+  {
+    path: "quiz",
+    component: QuizComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    QuizComponent
+    QuizComponent,
+    HomeComponent,
+    AppNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
